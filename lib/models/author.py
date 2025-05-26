e47e9dbe1634d192c5633622e8bd130cb4d81cd2
Kassim <kassimrooney@gmail.com>
2025-05-26 20:54:13 +0300

    Implemented Author model

---
+++ lib/models/author.py
@@ -0,0 +1,26 @@
+from lib.db.connection import get_connection
+
+class Author:
+    def __init__(self, id=None, name=""):
+        self.id = id
+        self.name = name
+
+    def save(self):
+        conn = get_connection()
+        cursor = conn.cursor()
+        if self.id is None:
+            cursor.execute("INSERT INTO authors (name) VALUES (?)", (self.name,))
+            self.id = cursor.lastrowid
+        else:
+            cursor.execute("UPDATE authors SET name = ? WHERE id = ?", (self.name, self.id))
+        conn.commit()
+        conn.close()
+
+    @classmethod
+    def find_by_id(cls, id):
+        conn = get_connection()
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM authors WHERE id = ?", (id,))
+        row = cursor.fetchone()
+        conn.close()
+        return cls(id=row["id"], name=row["name"]) if row else None
