7f0f6c25c753a6c6da4bfdb91e8aab7aa2fd6038
Kassim <kassimrooney@gmail.com>
2025-05-27 10:38:24 +0300

    Runned testfiles

--- tests/test_author.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from lib.models.author import Author
-
-def test_save_and_find_author():
-    author = Author(name="Test Author")
-    author.save()
-
-    found = Author.find_by_id(author.id)
-    assert found.name == "Test Author"
